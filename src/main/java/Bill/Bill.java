030f7e9c9581f76cd078f52510f8e6d160527d48
Jens Verelst <jensverelst@gmail.com>
2022-11-17 17:02:03 +0100

    regular update

--- src/main/java/Bill/Bill.java
+++ src/main/java/Bill/Bill.java
@@ -1,14 +1,5 @@
 package Bill;
 
-<<<<<<< HEAD
-import Tickets.Ticket;
-
-import java.util.ArrayList;
-
 public class Bill {
-    private ArrayList<String> indebted;
-    private Ticket ticket;
-=======
-public class Bill {
->>>>>>> master
+
 }
