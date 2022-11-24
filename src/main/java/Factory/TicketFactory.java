7376b86b259f7488fa7ce447f25e96e1550e5914
Jens Verelst <jensverelst@gmail.com>
2022-11-24 13:56:59 +0100

    updated ticketfactory

--- src/main/java/Factory/TicketFactory.java
+++ src/main/java/Factory/TicketFactory.java
@@ -11,20 +11,20 @@ public class TicketFactory {
     public boolean isEven(TicketTypes type){
         switch(type){
             case TaxiTicket: return true;
-            case RestaurantTicket: return true;
+            case RestaurantTicket: return false;
             case AirplaneTicket: return true;
             case ConcertTicket: return true;
-            case OtherTicket: return true;
+            case OtherTicket: return false;
         }
         return false;
     }
     public Ticket getTicket(Person creator, double amountUpfront, TicketTypes type){
         switch(type){
             case TaxiTicket: return new EvenTicket(creator, amountUpfront, type);
-            case RestaurantTicket: return new EvenTicket(creator,amountUpfront,type);
+            case RestaurantTicket: return new UnevenTicket(creator,amountUpfront,type);
             case AirplaneTicket: return new EvenTicket(creator,amountUpfront,type);
             case ConcertTicket: return new EvenTicket(creator,amountUpfront,type);
-            case OtherTicket: return new EvenTicket(creator,amountUpfront,type);
+            case OtherTicket: return new UnevenTicket(creator,amountUpfront,type);
         }
         return new UnevenTicket(creator,amountUpfront,type);
     }
