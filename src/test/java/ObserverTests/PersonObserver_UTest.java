0b3d9fe2cfa25adca73d7570637708bc80eac708
Alexander Bostyn <alexander.bostyn@gmail.com>
2022-12-18 22:48:30 +0100

    Forgot one test

--- src/test/java/ObserverTests/PersonObserver_UTest.java
+++ src/test/java/ObserverTests/PersonObserver_UTest.java
@@ -1,9 +1,11 @@
-/*package ObserverTests;
+package ObserverTests;
 
+import Controller.Controller;
 import Database.PersonDatabase;
 import Database.TicketDatabase;
 import Factory.TicketFactory;
 import Observers.ObserverTicket;
+import Controller.DatabaseController;
 import Tickets.Ticket;
 import Tickets.EvenTicket;
 import Tickets.UnevenTicket;
@@ -26,6 +28,7 @@ public class PersonObserver_UTest {
     Ticket t1 = f.getTicket(p1,120, TaxiTicket,"Taxi");
     Ticket t2 = f.getTicket(p2, 300, RestaurantTicket, "Restaurant");
     ObserverTicket observerTicket = new ObserverTicket();
+    Controller controller = new DatabaseController(dbp, dbt);
 
     public PersonObserver_UTest() {}
 
@@ -33,19 +36,17 @@ public class PersonObserver_UTest {
     public void Initialize()
     {
         dbt.addObserver(observerTicket);
-        dbp.addPerson(p1);
-        dbp.addPerson(p2);
-        dbp.addPerson(p3);
+        controller.addPerson(p1);
+        controller.addPerson(p2);
+        controller.addPerson(p3);
         t1.addPayer(p1);
         t1.addPayer(p2);
         t1.addPayer(p3);
-        t1.divideBill();
-        dbt.addEntry(t1);
+        controller.addEntry(t1);
         t2.addPayer(p1);
         t2.addPayer(p2);
         t2.addPayer(p3,200);
-        t2.divideBill();
-        dbt.addEntry(t2);
+        controller.addEntry(t2);
     }
 
     @Test
@@ -55,4 +56,4 @@ public class PersonObserver_UTest {
         assertThat("Correct amountPaid",210 == p2.getAmountPaid());
         assertThat("Correct amountPaid",-240 == p3.getAmountPaid());
     }
-}*/
+}
